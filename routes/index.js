bca329abda7c1c723b8c992674e520d5969c582d
Adel Wu <adelwu@seas.upenn.edu>
2019-08-09 16:52:06 -0500

    Authorization problems

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app = express()
+
+app.set('port', 3000);
+
+app.get('/token', function(req, resp) {
+  resp.header('Access-Control-Allow-Origin', '*');
+  resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+  var client_id = process.env.SPOTIFY_CLIENT_ID;
+  var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization:
+          'Basic ' +
+          new Buffer(client_id + ':' + client_secret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      resp.json({ token: body.access_token });
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 module.exports = router;
