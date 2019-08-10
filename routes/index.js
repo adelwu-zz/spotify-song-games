b426dd559e814ea10b7cb0676a3d5dfcdfc08a63
Adel Wu <adelwu@seas.upenn.edu>
2019-08-09 23:00:17 -0500

    Token response obtained

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 app = express()
 
-app.set('port', 3000);
+app.set('port', 3500);
 
-app.get('/token', function(req, resp) {
+app.get('/', function(req, resp) {
   resp.header('Access-Control-Allow-Origin', '*');
   resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
 
-  var client_id = process.env.SPOTIFY_CLIENT_ID;
-  var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  var client_id = 'a88350ce46434eb69ef3df2cab4a940f';
+  var client_secret = '16def6c1939b49e5bcab25cb5040c9fb';
 
   // your application requests authorization
   var authOptions = {
