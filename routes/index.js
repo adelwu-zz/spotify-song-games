8b7b3ff41e0750f51892678528bd4000c6a58878
Adel Wu <adelwu@seas.upenn.edu>
2019-08-16 13:47:32 -0500

    api stuffs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,92 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+const path = require("path");
+const Track = require('../utils/track');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app = express()
+
+app.set('port', 3500);
+
+let auth_token = '';
+const client_id = 'a88350ce46434eb69ef3df2cab4a940f';
+const client_secret = '16def6c1939b49e5bcab25cb5040c9fb';
+
+
+
+app.get('/', function(req, resp) {
+  resp.header('Access-Control-Allow-Origin', '*');
+  resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+  //getTop50();
+
+  //renders index file
+  resp.sendFile(path.join(__dirname, '../views/index.html'));
+
+});
+
+app.get('/songs', function(req, resp) {
+  let spotifyApi = new SpotifyWebApi();
+  let allTracks = [];
+
+  // your application requests authorization
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization':
+          'Basic ' +
+          new Buffer(client_id + ':' + client_secret).toString('base64')
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      auth_token = body.access_token;
+      spotifyApi.setAccessToken(auth_token);
+      console.log(auth_token);
+
+      var playlistTest = {
+        url: 'https://api.spotify.com/v1/playlists/37i9dQZEVXbLRQDuF5jeBp/tracks',
+        headers: {
+          Accept: "application/json",
+          'Content-Type': "application/json",
+          Authorization:
+              'Bearer ' + auth_token
+        },
+        json: true
+      };
+
+      // Get Top US 50 Playlist
+      spotifyApi.getPlaylist('37i9dQZEVXbLRQDuF5jeBp').then(
+          function (data) {
+            let response = data.body;
+            let tracks = response.tracks.items;
+            tracks.forEach(item => {
+              let uri = item.track.uri.split(":");
+              uri = uri[2];
+              let t = new Track(item.track.name, uri);
+              allTracks.push(t);
+            });
+            //console.log(allTracks);
+            resp.send(allTracks);
+          },
+          function (err) {
+            console.error(err);
+          }
+      );
+
+    }
+  });
+});
+
+
+let server = app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 module.exports = router;
